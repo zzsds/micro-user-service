80d8ef3be9b8cb3a941f137e3ee416ee0970d359
jayden-zhihui <737043980@qq.com>
2020-07-21 18:28:16 +0800

    更新用户服务

--- handler/user.go
+++ handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 
+	"github.com/golang/protobuf/ptypes"
 	"github.com/micro/go-micro/v2"
 	user "github.com/zzsds/micro-store/user-service/proto/user"
 	"github.com/zzsds/micro-store/user-service/service"
@@ -10,23 +11,72 @@ import (
 
 // User ...
 type User struct {
-	name string
+	dao     *service.Dao
+	name    string
+	service service.UserInterface
 }
 
 // NewUserHandler ...初始化Handler
-func NewUserHandler(srv micro.Service, db *service.Dao) *User {
+func NewUserHandler(srv micro.Service, dao *service.Dao) *User {
 	return &User{
-		name: srv.Name(),
+		dao:     dao,
+		name:    srv.Name(),
+		service: service.NewUser(dao.Db()),
 	}
 }
 
 // Index ...
 func (h *User) Index(ctx context.Context, req *user.Pagination, rsp *user.List) error {
+
+	if req.GetSize() <= 0 {
+		req.Size = 20
+	}
+	list, total := h.service.PageDate(req.GetPage(), req.GetSize(), req.GetCondition(), req.GetOrder())
+	req.Total = total
+
+	for _, model := range list {
+		createdAt, _ := ptypes.TimestampProto(model.CreatedAt)
+		updatedAt, _ := ptypes.TimestampProto(model.UpdatedAt)
+		birthday, _ := ptypes.TimestampProto(*model.Birthday)
+		rsp.Data = append(rsp.GetData(), &user.Resource{
+			CreatedAt: createdAt,
+			UpdatedAt: updatedAt,
+			Id:        int32(model.ID),
+			Name:      model.Name,
+			Mobile:    model.Mobile,
+			Code:      model.Code,
+			Source:    model.Source,
+			Birthday:  birthday,
+			Enabled:   user.Enabled(model.Enabled),
+		})
+	}
+
 	return nil
 }
 
 // Show ...
 func (h *User) Show(ctx context.Context, req *user.ShowRequest, rsp *user.ShowResponse) error {
+	if req.GetId() <= 0 {
+		// return errors.BadRequest()
+	}
+	model := h.service.FindID(req.GetId())
+	if model == nil {
+		return nil
+	}
+	createdAt, _ := ptypes.TimestampProto(model.CreatedAt)
+	updatedAt, _ := ptypes.TimestampProto(model.UpdatedAt)
+	birthday, _ := ptypes.TimestampProto(*model.Birthday)
+	*rsp.Data = user.Resource{
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+		Id:        int32(model.ID),
+		Name:      model.Name,
+		Mobile:    model.Mobile,
+		Code:      model.Code,
+		Source:    model.Source,
+		Birthday:  birthday,
+		Enabled:   user.Enabled(model.Enabled),
+	}
 	return nil
 }
 
--- service/user.go
+++ service/user.go
@@ -7,9 +7,11 @@ import (
 
 // UserInterface ...
 type UserInterface interface {
+	PageDate(page, size int32, condition, order []string) ([]*models.User, int32)
 	GetMobile(mobile string) *models.User
 	GetEmail(email string) *models.User
 	Create(*models.User) error
+	FindID(id int32) *models.User
 }
 
 // User ...
@@ -22,6 +24,25 @@ func NewUser(DB *gorm.DB) UserInterface {
 	return &User{
 		db: DB,
 	}
+}
+
+// FindID ...
+func (s *User) FindID(id int32) *models.User {
+	model := models.User{}
+	s.db.First(&model)
+	return &model
+}
+
+// PageDate 分页查询
+func (s *User) PageDate(page, size int32, condition, order []string) (list []*models.User, total int32) {
+	for _, o := range order {
+		s.db = s.db.Order(o)
+	}
+	for _, c := range condition {
+		s.db = s.db.Where(c)
+	}
+	s.db.Model(models.User{}).Count(&total).Offset(page * size).Limit(size).Find(&list)
+	return list, total
 }
 
 // GetMobile ...
